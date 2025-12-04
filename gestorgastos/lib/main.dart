a6cd3592b3a7f0f7cc76147ac70c4ed3d4a7f638
AngelMaroto <angel.margar@educa.jcyl.es>
2025-12-04 12:31:31 +0100

    1

--- gestorgastos/lib/main.dart
+++ gestorgastos/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path/path.dart';
 
-void main() {
+void main() async{
+  // Inicializar sqflite para escritorio
+  sqfliteFfiInit();
+  final databaseFactory = databaseFactoryFfi;
+  // Ruta para la base de datos
+  final dbPath = join(await databaseFactory.getDatabasesPath(), 'form.db');
+  final database = await databaseFactory.openDatabase(dbPath);
+  // Crear tabla de tareas si no existe
+  await database.execute('''
+    CREATE TABLE IF NOT EXISTS forms (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    edad TEXT NOT NULL
+    )
+    ''');
   runApp(const InsertarDato());
 }
 
@@ -29,6 +46,11 @@ class InsertarDato extends StatefulWidget {
 class _InsertarDatoState extends State<InsertarDato> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("Insertar Datos"),),
+      body: Center(
+
+      ),
+    );
   }
 }
